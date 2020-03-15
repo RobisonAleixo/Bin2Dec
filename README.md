d39f4ca4148c57ed8c179b3dfb4fcfee6acce7ab
Robison Aleixo <robison.wilson@gmail.com>
2020-03-15 15:03:52 -0300

    Validacao de dados adicionado no front-end

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bin2Dec
 Bin2Dec é uma das ideias de projetos do repositório: https://github.com/florinpop17/app-ideas
+
+Foi usado node como back-end nesse projeto, embora não há necessidade alguma para realizar tal conversão, binário para Decimal.
--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,39 @@
 document.binaryForm.onsubmit = async e => {
     e.preventDefault();
 
-    const form = e.target;
-    const data = new FormData(form);
+    let binaryOnly = () => {
+        const binary = document.querySelector('input').value;
 
-    const options = {
-        method: form.method,
-        body: new URLSearchParams(data)
+        for(let i = 0; i < binary.length; i++) {
+            if (binary.charAt(i) != '0' && binary.charAt(i) != '1' ) {
+                alert("Insert only 0 and 1!")
+                document.querySelector('.result').innerHTML = "Decimal";
+                document.querySelector('input').value = "";
+                return false;
+            } 
+        }
+
+        return true;
     };
 
-    fetch(form.action, options)
-        .then(resp => resp.json())
-        .then(json => {
-            const resultado = document.querySelector('.result');
-            resultado.innerHTML = JSON.stringify(json.decimal);
-        });
+    if(binaryOnly()) {
+        const form = e.target;
+        const data = new FormData(form);
+    
+        const options = {
+            method: form.method,
+            body: new URLSearchParams(data)
+        };
+    
+        fetch(form.action, options)
+            .then(resp => resp.json())
+            .then(json => {
+                const resultado = document.querySelector('.result');
+                resultado.innerHTML = JSON.stringify(json.decimal);
+            });
+    } else {
+        console.log('foi');
+    }
+
+
 }
